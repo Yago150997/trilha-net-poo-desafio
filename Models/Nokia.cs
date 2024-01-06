f55f8a69a782a19e4fddf7c62f46bf26813dc0fe
Yago Gama <yagoggb97@gmail.com>
2024-01-06 12:19:38 -0300

    Adicionado classe filha 'nokia' com o construtor e declarando a função InstalarAplicativo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string model, string imei, int memoria) : base( numero,  model,  imei, memoria)
+        {
+            Console.WriteLine("Smartphone Nokia: ");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp}...");
+        }
+
     }
-}
+    }
