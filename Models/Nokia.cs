5e48d2892e891d9a3a6d9c9510c4765ca3fa1d2f
Yago Gama <yagoggb97@gmail.com>
2024-01-06 13:05:07 -0300

    Editado arquivo Program.cs para testar as classes filhas. Projeto concluido.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,13 +1,13 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
+    
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string model, string imei, int memoria) : base( numero,  model,  imei, memoria)
         {
             Console.WriteLine("Smartphone Nokia: ");
         }
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando o aplicativo {nomeApp}...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone Nokia = new Nokia(numero: "55893321", model: "M1", imei: "2225559", memoria: 256);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Netflix");
+Console.WriteLine();
+
+Smartphone Iphone = new Iphone(numero: "99315522", model: "M2", imei: "5423871235", memoria: 128);
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Prime Video");
